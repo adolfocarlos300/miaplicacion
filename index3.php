bb73d4bd39734613f59cc02842aeeaa27ae10eeb
adolfo300 <adolfo_carlos300@hotmail.com>
2020-03-25 23:51:21 -0500

    github 5

---
+++ index3.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Material Design for Bootstrap</title>
+  <!-- MDB icon -->
+  <link rel="icon" href="img/mdb-favicon.ico" type="image/x-icon">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+  <!-- Google Fonts Roboto -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+  <!-- Bootstrap core CSS -->
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <!-- Your custom styles (optional) -->
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+  <!-- Start your project here-->  
+
+<div class="container">
+  <div class="row align-items-start example-row mb-3 example-parent" style="min-height: 10rem;">
+    <div class="col-sm-3 col-example">
+      One of three columns
+    </div>
+    <div class="col-sm-3 col-example">
+      One of three columns
+    </div>
+    <div class="col-sm-6 col-example">
+      One of three columns
+    </div>
+  </div>
+  <div class="row align-items-center example-row mb-3 example-parent" style="min-height: 10rem;">
+    <div class="col col-example">
+      One of three columns
+    </div>
+    <div class="col col-example">
+      One of three columns
+    </div>
+    <div class="col col-example">
+      One of three columns
+    </div>
+  </div>
+  <div class="row  align-items-end example-row mb-3 example-parent" style="min-height: 10rem;">
+    <div class="col col-example">
+      One of three columns
+    </div>
+    <div class="col col-example">
+      One of three columns
+    </div>
+    <div class="col col-example">
+      One of three columns
+    </div>
+  </div>
+</div>
+  <!-- End your project here-->
+
+  <!-- jQuery -->
+  <script type="text/javascript" src="js/jquery.min.js"></script>
+    <!-- Bootstrap core JavaScript -->
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+  
+  
+</body>
+</html>
