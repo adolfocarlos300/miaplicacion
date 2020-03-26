1bd26f1b98c829ad357f851ef06163f873a18b33
adolfo300 <adolfo_carlos300@hotmail.com>
2020-03-25 22:20:18 -0500

    adolfo3

---
+++ index2.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Material Design for Bootstrap</title>
+  <!-- MDB icon -->
+  <link rel="icon" href="img/mdb-favicon.ico" type="image/x-icon">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+  <!-- Google Fonts Roboto -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+  <!-- Bootstrap core CSS -->
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <!-- Your custom styles (optional) -->
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+  <!-- Start your project here-->  
+
+  <div class="container">
+    <div class="row"  style="border:6px yellow solid;">
+      <div class="col-md-4">.col-md-4</div>
+      <div class="col-md-4 offset-md-4">.col-md-4 .offset-md-4</div>
+    </div>
+    <div class="row" style="border:6px red solid;">
+      <div class="col-md-3 offset-md-3">.col-md-3 .offset-md-3</div>
+      <div class="col-md-3 offset-md-3">.col-md-3 .offset-md-3</div>
+    </div>
+    <div class="row" style="border:6px pink solid;">
+      <div class="col-md-6 offset-md-3">.col-md-6 .offset-md-3</div>
+    </div>
+  </div>
+  
+<div class="row mx-md-n5">
+  <div class="col px-md-5"><div class="p-3 border bg-light">Custom column padding</div></div>
+  <div class="col px-md-5"><div class="p-3 border bg-light">Custom column padding</div></div>
+</div>
+
+<div class="container">
+  <div class="row border bg-light">
+    <div class="col">
+      1 of 2
+    </div>
+    <div class="col">
+      2 of 2
+    </div>
+  </div>
+  <div class="w-100"></div>
+  <div class="row border bg-light">
+    <div class="col">
+      1 of 3
+    </div>
+    <div class="col">
+      2 of 3
+    </div>
+    <div class="col">
+      3 of 3
+    </div>
+  </div>
+</div>
+
+
+<div class="container">
+  <div class="row" style="border:6px yellow solid;">
+    <div class="col">col</div>
+    <div class="col">col</div>
+    <div class="w-100"></div>
+    <div class="col">col</div>
+    <div class="col">col</div>
+  </div>
+</div>
+
+<div class="container">
+  <div class="row align-items-start example-row mb-3 example-parent" style="min-height: 10rem;">
+    <div class="col-sm-3 col-example">
+      One of three columns
+    </div>
+    <div class="col-sm-3 col-example">
+      One of three columns
+    </div>
+    <div class="col-sm-6 col-example">
+      One of three columns
+    </div>
+  </div>
+  <div class="row align-items-center example-row mb-3 example-parent" style="min-height: 10rem;">
+    <div class="col col-example">
+      One of three columns
+    </div>
+    <div class="col col-example">
+      One of three columns
+    </div>
+    <div class="col col-example">
+      One of three columns
+    </div>
+  </div>
+  <div class="row  align-items-end example-row mb-3 example-parent" style="min-height: 10rem;">
+    <div class="col col-example">
+      One of three columns
+    </div>
+    <div class="col col-example">
+      One of three columns
+    </div>
+    <div class="col col-example">
+      One of three columns
+    </div>
+  </div>
+</div>
+  <!-- End your project here-->
+
+  <!-- jQuery -->
+  <script type="text/javascript" src="js/jquery.min.js"></script>
+    <!-- Bootstrap core JavaScript -->
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+  
+  
+</body>
+</html>
